09cf82e33f20ccd76796e86090135b1601749474
Tanishq <93016588+Technishq@users.noreply.github.com>
2024-03-13 20:19:06 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Set working directory
+setwd('C:/Users/tanishq/Documents/Coursera-R')
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# Define a function to create a cache matrix
+makeCacheMatrix <- function(x = matrix(sample(1:100, 9), 3, 3)) {
+  s <- NULL
+  
+  # Set the matrix value
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  
+  # Get the matrix value
+  get <- function() x
+  
+  # Set the solved value
+  setsolve <- function(solve) s <<- solve
+  
+  # Get the solved value
+  getsolve <- function() s
+  
+  # Return list of functions
+  list(set = set, 
+       get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+# Define a function to cache solving of matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if (!is.null(s)) {
+    message("getting inversed matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
